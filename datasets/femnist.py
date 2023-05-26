a75fdcc35308158196f316e5769999ea0b99a2fd
Iman Morovatian <mo.iman.1377@gmail.com>
2023-05-26 14:57:16 +0200

    convert image from list into numpyarray of size (28,28,1)

--- datasets/femnist.py
+++ datasets/femnist.py
@@ -5,6 +5,7 @@ from typing import Any
 from torch.utils.data import Dataset
 
 IMAGE_SIZE = 28
+IMAGE_CHANNELS = 1
 
 # transform = transforms.Compose([
 #     transforms.ToTensor(),
@@ -23,12 +24,14 @@ class Femnist(Dataset):
     def __getitem__(self, index: int) -> Any:
         # TODO: missing code here! -----------> ##### DONE :)
         image = self.samples[index][0]
-        label = self.samples[index][1]
+        image = np.array(image, dtype=np.float32).reshape(IMAGE_SIZE, IMAGE_SIZE, IMAGE_CHANNELS)
 
+        label = self.samples[index][1]
+        
         if self.transform is not None:
             image = self.transform(image)
 
-        return (image, label)
+        return image, label
 
     def __len__(self) -> int:
         return len(self.samples)
