f99357ed7f6e5d88c975619594d9dec6e544468b
Iman Morovatian <mo.iman.1377@gmail.com>
2023-06-03 21:47:55 +0200

    update cnn.py update cnn.py to be the same as the model in centralized_training.py Changes include adding batch normalization convolution layers and drop out to fully connected ones

--- centralized_training.py
+++ centralized_training.py
@@ -4,50 +4,8 @@ import torch.optim as optim
 from torch.optim.lr_scheduler import ExponentialLR
 from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
+from models.cnn import CNN
 import wandb
-
-
-class CNN(nn.Module):
-
-    def __init__(self, in_channels=1, num_classes=62):
-        super(CNN, self).__init__()
-
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(in_channels, 32, kernel_size=5, padding=2),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=5, padding=2),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2) 
-        )
-
-        self.fc1 = nn.Sequential(
-            nn.Dropout(0.4),
-            nn.Linear(64*7*7, 2048),
-            nn.ReLU()
-        )
-
-        self.fc2 = nn.Sequential(
-            nn.Dropout(0.8),
-            nn.Linear(2048, num_classes)
-        )
-
-
-    def forward(self, X):
-        out = self.layer1(X)
-        out = self.layer2(out)
-
-        out = out.reshape(out.size(0), -1)
-
-        out = self.fc1(out)
-        out = self.fc2(out)
-        
-        return out
 
 
 def train(data_loader, net, loss_function, optimizer, device):
--- models/cnn.py
+++ models/cnn.py
@@ -1,4 +1,4 @@
-import torch.nn as nn
+import torch.nn as nn 
 
 
 class CNN(nn.Module):
@@ -8,22 +8,26 @@ class CNN(nn.Module):
 
         self.layer1 = nn.Sequential(
             nn.Conv2d(in_channels, 32, kernel_size=5, padding=2),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2)
         )
 
         self.layer2 = nn.Sequential(
             nn.Conv2d(32, 64, kernel_size=5, padding=2),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2) 
         )
 
         self.fc1 = nn.Sequential(
+            nn.Dropout(0.4),
             nn.Linear(64*7*7, 2048),
             nn.ReLU()
         )
 
         self.fc2 = nn.Sequential(
+            nn.Dropout(0.8),
             nn.Linear(2048, num_classes)
         )
 
