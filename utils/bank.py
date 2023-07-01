8870998554b9ad13ed7d1c1efb4f47d55f6af2db
Iman Morovatian <mo.iman.1377@gmail.com>
2023-07-01 21:43:53 +0200

    edit get_amps in bank.py sometimes there is no image with same label among participating clients to generate counterparts. In this situtation, other clients in the bank not participating in training are used.

--- utils/bank.py
+++ utils/bank.py
@@ -37,16 +37,36 @@ class Bank:
     def get_amps(self, client_name, image_label, other_client_names):
         image_label = int(image_label)
         collected_amps = []
-        for client, amps in self.bank.items():
-            if client != client_name and client in other_client_names:
-                if image_label in amps.keys():
-                    if len(amps[image_label]) > 1:
-                        all_indices = range(len(amps[image_label]))
+        alternative_amps = []
+        limit = len(other_client_names)
+
+        for name in other_client_names:
+            if name != client_name:
+                if image_label in self.bank[name].keys():
+                    if len(self.bank[name][image_label]) > 1:
+                        all_indices = range(len(self.bank[name][image_label]))
                         index = np.random.choice(all_indices, 1)[0]
-                        sample = amps[image_label][index]
+                        sample = self.bank[name][image_label][index]
                     else:
-                        sample = amps[image_label][0]
+                        sample = self.bank[name][image_label][0]
+
                     collected_amps.append(sample)
 
+        
+        if len(collected_amps) == 0:
+            for client, amps in self.bank.items():
+                if len(alternative_amps) <= limit and client != client_name:
+                    if image_label in amps.keys():
+                        if len(amps[image_label]) > 1:
+                            all_indices = range(len(amps[image_label]))
+                            index = np.random.choice(all_indices, 1)[0]
+                            sample = amps[image_label][index]
+                        else:
+                            sample = amps[image_label][0]
 
-        return  collected_amps
+                        alternative_amps.append(sample)
+
+
+            return  alternative_amps
+        else:
+            return collected_amps
